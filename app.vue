8c14ee0b37b0e84ece359082f9f7d4bc9352ff42
Akayy <techakayy@gmail.com>
2024-08-31 14:35:25 +1000

    useImage vuetify example

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  const src =
+    'https://images.unsplash.com/photo-1654870468927-92c943da24fe?crop=entropy&cs=srgb&fm=jpg&ixid=M3wyMDkyMnwwfDF8c2VhcmNofDQ5fHxiYWtlcnklMjBjYWtlfGVufDB8fHx8MTY5NTA5NzYyNXww&ixlib=rb-4.0.3&q=85'
+  const height = 500
+  const img = useImage()
+  const _srcset = computed(() => {
+    return img.getSizes(src, {
+      sizes: 'xs:100vw sm:100vw md:100vw lg:100vw xl:100vw',
+      modifiers: {
+        format: 'webp',
+        quality: 70,
+        height,
+      },
+    })
+  })
+</script>
+
+<template>
+  <v-img
+    :lazy-src="img(src, {width: 10, quality: 70})"
+    :src="img(src, {height, quality: 70})"
+    :srcset="_srcset.srcset"
+    :height="height"
+    :sizes="_srcset.sizes"></v-img>
+</template>
